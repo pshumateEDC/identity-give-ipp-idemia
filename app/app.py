3c0a6afb18254956c294ae463a8c3d3f7fb6a3fe
pshumateEDC <pshumate@easydynamics.com>
2020-12-17 10:49:18 -0500

    remove white space

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@
 import re
 from uuid import UUID
 from http import HTTPStatus
-from chalice import Chalice, Response
+from chalice import Chalice, Response, Error
 from jsonschema import validate, ValidationError
 from chalicelib import REG_SCHEMA, STATUS_SCHEMA, idemia_service
 
@@ -21,6 +21,7 @@ def enrollment_register():
     try:
         validate(data, REG_SCHEMA)
     except ValidationError:
+        raise
         return response(HTTPStatus.BAD_REQUEST, {"error": "Invalid Request Body"})
 
     # validate UUID
